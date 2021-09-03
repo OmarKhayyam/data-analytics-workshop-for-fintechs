49c29b562db872e78377331c5c58a57461ea9f5c
EC2 Default User <ec2-user@ip-172-31-84-11.ec2.internal>
2021-09-03 06:03:48 +0000

    first commit for datageneration

---
+++ datagen/generatedata.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+
+import random 
+import mysql.connector
+from mysql.connector import errorcode
+
+userid = []
+city = ['BOM','DEL','BLR','CAL','MAA','GOI','COK','HYD','PNQ','SXR','TRV','AMD','ZER']
+trxtype = ['CREDIT','DEBIT','NONMON']
+inrvalue = 0 ## An INR value between 100.00 and 10000000.00
+features = dict()
+features = {0: 'fundtransfer',1: 'upi',2: 'enquiry',3: 'forex',4: 'mutualfund-investment',5: 'capitalmarket-investment',6: 'FD',7: 'PensionFund-investment'}
+
+def generateaccnnos():
+    '''Returns a list of account numbers'''
+    return random.sample(range(100000,1000000),500000)
+
+# We now create a table with the following fields
+# userid|city|transactiontype|inrvalue|timeinapp|featureused|timestamp
+
+## Some constraints that the data should be always met by the data, else, this needs
+## investigation.
+## The app/account cannot be accessed by the same user from different cities within an hour.
+## There shouldn't be an overlap in app usage between two sessions for the same account.
+## If the a customer tries to withdraw more than the amount available in the account, the amount
+#    should not be operative for 24 hrs after the incident, and the customers should be informed of this.
+## If a customer never does forex transactions but does it this time, the customer has to enable this
+#    facility, customer service should call this customer to find out the reason for this and enable the service.
+## A customer should not spend the maximum limit on UPI transactions on 3 consecutive days.
+
+### Some patterns that need to be observed and leveraged.
+## Multiple enquiries implies the customer is looking to buy a product.
+## Regular forex transactions requires continuous scrutiny, elevated relationship engagement with an RM.
+## Regular MF investor, may be a high value customer.
+## Large cpaital market investor may be a high value customer.
+## Regular pension fund investor may need other investment products that have a better balance of risk and reward.
+
+lastAccnNo = 000000
+DB_NAME = 'workshopdb1'
+TABLES = {}
+TABLES['customeractivity'] = (
+        "CREATE TABLE `customeractivity` ("
+        "`user_id` int(11) NOT NULL, "
+        "`city` varchar(3) NOT NULL, "
+        "`transaction_type` enum('CREDIT','DEBIT','NON_MONETARY'), "
+        "`monetary_value` decimal(8,2) NOT NULL, "
+        "`timeinapp` smallint NOT NULL, "
+        "`feature_used` enum('FUNDTRANSFER','UPITRANSACTION','ENQUIRY','FOREX','MF-INVEST','CAPITALMARKET-INVEST','FD-INVEST','PENSIONFUND-INVEST') NOT NULL, "
+        "ts TIMESTAMP, "
+        "PRIMARY KEY (`user_id`))"
+        )
+
+TABLES['customers'] = (
+        "CREATE TABLE `customers` ("
+        "`user_id` int(11) NOT NULL," 
+        "PRIMARY KEY (`user_id`))"
+        )
+
+def persistAccns():
+    print("Setting up accounts...")
+    accnslist = generateaccnnos()
+    ## We already expect the workshopdb1 to exist
+    conn = mysql.connector.connect(user='admin',password='master123', host='database-2-instance-1.ckyzeld8ogd5.us-east-1.rds.amazonaws.com')
+    cursor = conn.cursor()
+    try:
+        print("Creating the table structure...")
+        cursor.execute("USE {}".format(DB_NAME))
+    except mysql.connector.Error as err:
+        print("Database {} does not exists.".format(DB_NAME))
+        if err.errno == errorcode.ER_BAD_DB_ERROR:
+            print(err)
+            exit(1)
+    try:
+        print("Creating tables {} and {}".format("customeractivity","customers"))
+        cursor.execute(TABLES['customeractivity'])
+        cursor.execute(TABLES['customers'])
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+            print("already exists.")
+        else:
+            print(err.msg)
+    else:
+        print("OK - tables created.")
+    with open('account_ids.txt', 'w') as f:
+        for item in accnslist:
+            f.write("%s\n" % item)
+    try:
+        cursor.execute("LOAD DATA FROM S3 's3://workshopdb1-input/DBINPUT/account_ids.txt' INTO TABLE customers (user_id)")
+    except:
+        print(err.msg)
+        cursor.close()
+        conn.close
+        exit(1)
+
+    cursor.close()
+    conn.close()
+
+persistAccns()
