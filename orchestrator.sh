988e201ac45d5b33b8b355c58d8f3e51123534d2
Rahul Shringarpure <shringar@amazon.com>
2021-09-08 08:15:42 +0530

    orchestrator.sh and template added.

--- orchestrator.sh
+++ orchestrator.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ## Instantiate CF Template
-aws cloudformation create-stack --stack-name datagenstack --template-body file://$PWD/datagen/DataLakeLabPrep.yml
-s3bucketname=`aws cloudformation describe-stacks --stack-name datagenstack | python3 -c "import sys, json; print(json.load(sys.stdin)['Stacks'][0]['Outputs'][OutputValue])"`
-echo $s3bucketname
+aws cloudformation create-stack --stack-name datagenstack --template-body file://$PWD/datagen/DataLakeLabPrep.yml --capabilities CAPABILITY_IAM 
+s3bucketname=`aws cloudformation describe-stacks --stack-name datagenstack | python3 -c "import sys, json; print(json.load(sys.stdin)['Stacks'][0]['Outputs'][0][OutputValue])"`
+ec2instance=`aws cloudformation describe-stacks --stack-name datagenstack | python3 -c "import sys, json; print(json.load(sys.stdin)['Stacks'][0]['Outputs'][1][OutputValue])"`
+echo $s3bucketname ec2instance
