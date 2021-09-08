b15c5a4f7da4cbd74ba9df061269d280bdb7d32b
Rahul Shringarpure <shringar@amazon.com>
2021-09-08 11:49:29 +0530

    changed orchestrator

--- orchestrator.sh
+++ orchestrator.sh
@@ -9,12 +9,5 @@ s3bucketname=`aws cloudformation describe-stacks --stack-name datagenstack | pyt
 
 ec2instance=`aws cloudformation describe-stacks --stack-name datagenstack | python3 -c "import sys, json; print(json.load(sys.stdin)['Stacks'][0]['Outputs'][1]['OutputValue'])"`
 
-echo $s3bucketname $ec2instance
+echo "S3 Bucket: "$s3bucketname" EC2 Instance Id: "$ec2instance
 
-aws ssm send-command --instance-ids $ec2instance --document-name "AWS-RunShellScript" --targets '[{"Key"="instanceids","Values"=["$ec2instance"]}]' --parameters '{"commands":["#!/bin/bash","yum -y update","yum install -y mysql57","yum install -y git","cd /home/ec2-user","git clone https://github.com/OmarKhayyam/data-lake-aws.git"]}' 
-
-#cmdid=`aws ssm send-command --instance-ids $ec2instance --document-name "AWS-RunShellScript" --comment "Git clone repo" --targets "Key=instanceids,Values=$ec2instance" --parameters 'commands=["cd /home/ec2-user","git clone https://github.com/OmarKhayyam/data-lake-aws.git"]' | python3 -c "import sys, json; print(json.load(sys.stdin)['Command']['CommandId'])"`
-
-#echo $cmdid
-
-#aws ssm command-executed --command-id $cmdid --instance-id $ec2instance
