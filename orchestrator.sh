da2443e552f270bab95361e819ca013b39a694a7
Rahul Shringarpure <shringar@amazon.com>
2021-09-10 11:17:27 +0530

    fixed shell script

--- orchestrator.sh
+++ orchestrator.sh
@@ -5,23 +5,4 @@ aws cloudformation create-stack --stack-name datagenstack --template-body file:/
 
 aws cloudformation wait stack-create-complete --stack-name datagenstack
 
-counter=0
-
-while [ $counter -lt 3 ]
-do
-	if [`aws cloudformation describe-stacks --stack-name datagenstack | python3 -c "import sys, json; print(json.load(sys.stdin)['Stacks'][0]['Outputs'][$counter]['OutputKey'])"` = "SourceAndDataBucket"]
-	then
-		s3bucketname=`aws cloudformation describe-stacks --stack-name datagenstack | python3 -c "import sys, json; print(json.load(sys.stdin)['Stacks'][0]['Outputs'][$counter]['OutputValue'])"`
-	elif [`aws cloudformation describe-stacks --stack-name datagenstack | python3 -c "import sys, json; print(json.load(sys.stdin)['Stacks'][0]['Outputs'][$counter]['OutputKey'])"` = "DBEndpoint"]
-	then
-		dbep=`aws cloudformation describe-stacks --stack-name datagenstack | python3 -c "import sys, json; print(json.load(sys.stdin)['Stacks'][0]['Outputs'][$counter]['OutputValue'])"`
-	else
-		ec2instance=`aws cloudformation describe-stacks --stack-name datagenstack | python3 -c "import sys, json; print(json.load(sys.stdin)['Stacks'][0]['Outputs'][$counter]['OutputValue'])"`
-	fi
-	counter=$counter+1
-done
-
-echo "S3 Bucket: "$s3bucketname 
-echo "EC2 Instance Id: "$ec2instance
-echo "Database Endpoint: "$dbep
-
+aws cloudformation describe-stacks --stack-name datagenstack | python3 -c "import sys, json; print(json.dumps(json.load(sys.stdin)['Stacks'][0]['Outputs'], indent = 3))"
